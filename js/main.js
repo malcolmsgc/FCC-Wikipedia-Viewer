0267d4da5b7fbf71e8ac1f40d9074f8f6ae4caf6
Malcolm Cumming <bemjogar@gmail.com>
2017-01-29 16:25:44 +0000

    Added base structure for JSONP refactoring

--- js/main.js
+++ js/main.js
@@ -1,27 +1,71 @@
-const fetchData = function (url){
-    const request = new XMLHttpRequest();
-    if (!request) {
-      alert('Error: Could not create http request object');
-      return false;
-    }
-    request.onreadystatechange = displayData;
-    request.open('GET', url);
-    request.send();
+//OBJECT HOLDING API QUERY COMPONENTS
+//https://www.mediawiki.org/wiki/API:Search
+//https://www.mediawiki.org/wiki/API:Cross-site_requests
+
+//OBJECT TO HOLD PARAMS FOR SEARCH QUERY
+const apiUrl = {
+  baseUrl:    'https://en.wikipedia.org/w/api.php?',
+  fixed: {
+    action:   'action=query',
+    format: 'format=json&formatversion=2',
+  },
+  canSet: {
+    srsearch: "srsearch=",
+    searchwhat: 'srwhat=',
+    srprop: 'srprop=',
+    srlimit:  'srlimit=', //10
+    callback: 'callback='
+  },
+  defaults: {
+    searchwhat: 'title|nearmatch',
+    srprop: 'snippet|titlesnippet',
+    srlimit:  '10',
+    callback: 'displayResults'  //function name without ()
+  },
+  createUrl( searchTerm, callback = this.defaults.callback ) {
+    const call =  `${this.baseUrl}
+                  ${this.fixed.action}
+                  ${this.fixed.format}
+                  ${}
+                  ${}
+                  ${}
+                  `;
   }
 }
 
-const spinnerContainer = document.querySelector(); // wherever spinner is to load
 
-function displayData() {
-    while (httpRequest.readyState !== XMLHttpRequest.DONE) {
-        spinnerContainer.classList.add('spinner');
+// JSONP example
+const JSONP = (function(){
+  let that = {};
+
+  that.send = function(src, options) {
+    let callback_name = options.callbackName || 'callback',
+      on_success = options.onSuccess || function(){},
+      on_timeout = options.onTimeout || function(){},
+      timeout = options.timeout || 10; // sec
+
+    var timeout_trigger = window.setTimeout(function(){
+      window[callback_name] = function(){};
+      on_timeout();
+    }, timeout * 1000);
+
+    window[callback_name] = function(data){
+      window.clearTimeout(timeout_trigger);
+      on_success(data);
     }
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        spinnerContainer.classList.remove('spinner');  
-        console.log(httpRequest.responseText);
-      } else {
-        alert('Error: unable to retrieve results');
-      }
-    }
+
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = src;
+
+    document.getElementsByTagName('head')[0].appendChild(script);
   }
+
+  return that;
+})();
+//end of JSONP example
+
+const spinnerContainer = document.querySelector(); // wherever spinner is to load
+spinnerContainer.classList.add('spinner');
+//spinnerContainer.classList.remove('spinner');  
