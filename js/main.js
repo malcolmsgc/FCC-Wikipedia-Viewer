4787b2ea50dac2d33e196407d682387b50ce8f9b
Malcolm Cumming <bemjogar@gmail.com>
2017-02-06 00:22:20 +0000

    Removed unecessary comments

--- js/main.js
+++ js/main.js
@@ -103,55 +103,6 @@ class SearchWiki extends JsonP {
 let test = new SearchWiki( 'elvis', {srlimit: '5'} );
 
 
-
-
-
-
 //const spinnerContainer = document.querySelector(); // wherever spinner is to load
 //spinnerContainer.classList.add('spinner');
 //spinnerContainer.classList.remove('spinner');
-
-/*
-STORAGE - KILL WHEN COMPLETE
-const apiUrl = {
-  baseUrl:    'https://en.wikipedia.org/w/api.php?',
-  fixed: {
-    action:   'action=query',
-    format:   'format=json&formatversion=2',
-    list:     'list=search'
-  },
-  create( searchTerm, settingsObj = {} ) {
-
-    //exception handling
-    try {
-      if (searchTerm === undefined) {throw new ReferenceError("searchTerm is undefined");}
-      if (typeof searchTerm !== 'string') {throw new TypeError("searchTerm should be a string");}
-      if (typeof settingsObj !== 'object') {throw new TypeError("searchTerm should be an object");}
-    }
-    catch(e) {
-       console.log(e);
-       //return e; //use instead of consolelog if risk that typeerror breaks call
-    }
-    //end exception handling
-
-    const { srwhat = 'text',
-            srprop = 'snippet|titlesnippet',
-            srlimit = '10',
-            callback =  'apiUrl.displayResults'  //function name without ()
-          } = settingsObj;  //destructure from settings object passed in. Take default if no matching key.
-    let call =  this.baseUrl +
-                `${this.fixed.action}&` +
-                `${this.fixed.format}&` +
-                `${this.fixed.list}&` +
-                `srsearch=${searchTerm}&` +
-                `callback=${callback}&` +
-                `srwhat=${srwhat}&` +
-                `srprop=${srprop}&` +
-                `srlimit=${srlimit}`;
-    return call;
-  },
-  displayResults(results){
-    console.log (results);
-  }
-};
-*/
